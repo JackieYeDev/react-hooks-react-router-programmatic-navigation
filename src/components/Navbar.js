d2c29cbbab71f08eb42a477dc2bdaf4b3e513b53
Jackie <jye0325@gmail.com>
2022-07-13 00:16:25 -0400

    commit tree cd0309b75796ff35788ea62a4fe3909735cf18e1

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,54 +1,59 @@
-import React from "react";
-import { NavLink, useHistory } from "react-router-dom";
+import React from 'react';
+import { NavLink, useHistory } from 'react-router-dom';
 
 const linkStyles = {
-  width: "100px",
-  padding: "12px",
-  margin: "0 6px 6px",
-  background: "blue",
-  textDecoration: "none",
-  color: "white",
+  width: '100px',
+  padding: '12px',
+  margin: '0 6px 6px',
+  background: 'blue',
+  textDecoration: 'none',
+  color: 'white',
 };
 
 function Navbar({ setIsLoggedIn }) {
   const history = useHistory();
 
+  // function handleClick() {
+  //   onLogout();
+  //   history.push('/login');
+  // }
+
   function handleLogout() {
     setIsLoggedIn(false);
-    history.push("/login");
+    history.push('/login');
   }
 
   return (
     <div>
       <NavLink
-        to="/"
+        to='/'
         /* set exact so it knows to only set activeStyle when route is deeply equal to link */
         exact
         /* add styling to Navlink */
         style={linkStyles}
         /* add prop for activeStyle */
         activeStyle={{
-          background: "darkblue",
+          background: 'darkblue',
         }}
       >
         Home
       </NavLink>
       <NavLink
-        to="/about"
+        to='/about'
         exact
         style={linkStyles}
         activeStyle={{
-          background: "darkblue",
+          background: 'darkblue',
         }}
       >
         About
       </NavLink>
       <NavLink
-        to="/login"
+        to='/login'
         exact
         style={linkStyles}
         activeStyle={{
-          background: "darkblue",
+          background: 'darkblue',
         }}
       >
         Login
